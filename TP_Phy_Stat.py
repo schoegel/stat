80c6603229dc4c5e1d11937858b01e9e23e7a369
Eliott-L <98524011+Eliott-L@users.noreply.github.com>
2022-04-28 23:38:11 +0200

    Add files via upload

--- TP_Phy_Stat.py
+++ TP_Phy_Stat.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math as math
 
+###
+
 y0 = 0.480
 
 A = 1.5554
@@ -16,89 +18,75 @@ mesures_volt = pd.read_excel ('Mesures positions.xlsx')     # On ouvre le tableu
 
 mesures_volt = np.asarray(mesures_volt).T       # On convertit le tableau issu du package "panda" en tableau numpy
 
-mesures_volt_coherent = []      # On créé un tableau cohérent où on élimine toutes les tensions inférieures à y0 (ce qui n'est pas sensé arriver,
-                                # cela correspond sans doute à un moment où le panneau réfléchissant s'est décalé de l'axe du capteur, et
-for i in range(len(mesures_volt)):      # le capteur s'est donc mis à viser dans le vide, d'où les valeurs trop basses
+mesures_volt_coherent = []      # On créé un tableau cohérent où on élimine toutes les tensions inférieures à y0
+                                # (ce qui n'est pas sensé arriver), cela correspond sans doute à un moment où
+                                # le panneau réfléchissant s'est décalé de l'axe du capteur,
+                                # le capteur se mettant donc à viser dans le vide, d'où les valeurs trop basses
+
+for i in range(len(mesures_volt)):
     mesures_volt_coherent.append(mesures_volt[i][np.where(mesures_volt[i]>y0)])
 
 mesures_distance = []       # Création d'un tableau de mesures de distance, où on convertit les volts en cm
 
+distances_normalisees = []
+
 for i in range(len(mesures_volt)):
-    mesures_distance.append([volt_to_cm(v) for v in mesures_volt_coherent[i]])
+    mesures_distance.append([volt_to_cm(v)/12.5 for v in mesures_volt_coherent[i]])
 
 noms_mesures = ['Mesure 1-2','Mesure 1-3', 'Mesure 2-2', 'Mesure 2-3', 'Mesure 3-3']
+
 subplots = [331,333,335,337,339]
 
 for i in range(len(noms_mesures)):
     plt.subplot(subplots[i])
     plt.xlabel('Distance')
     plt.ylabel('Nombre de mesures')
-    plt.hist(mesures_distance[i], bins = 20)
+    plt.hist(mesures_distance[i], bins = 20, density = True)
     plt.title(noms_mesures[i])
     plt.show()
 
 
-### modèle théorique 1 ( lattice gas model )
+### Modèle théorique 1 ( lattice gas model )
 
-# Nb =  # nombre de point
+d = 4       # largeur du ring
 
-Ng = 2 #nombre de boules à gauche
+l = 8       # longueur du ring
 
-Nd = 1 #nombre de boules à droite
+def P_stat(N):
 
-d = 4 #largeur du ring
+    Omega_x = []
+    Proba = []
 
-l = 8 #longueur du ring
+    for x in range(l+1):      # x correspond à la distance de la paroie par rapport au capteur, on étudie toutes les positions possibles
 
-x = 0 #distance capteur - barre
+        i = d*x         # surface du ring de gauche
+        j = d*(8-x)     # surface du ring de droite
 
-i = d*x #surface du premier ring (g)
+        OmegaG = math.comb(i,N[0])   # le nombre d'états(configurations) possibles a la gauche du systeme
+        OmegaD = math.comb(j,N[1])   # le nombre d'états(configurations) possibles a la droite du systeme
 
-j = d*(8-x) #taillle du second ring (d)
+        Omega_x.append(OmegaG * OmegaD)    # nombre total de configurations possible à un x donné
 
-OmegaG = math.comb(i*j,Ng)    #le nombre d'états(configurations) possible a la gauche du systeme
+    Omega = sum(Omega_x)
 
-OmegaD = math.comb(i*j,Nd)   #le nombre d'états(configurations) possible a la droite du systeme
+    for i in range(l+1):
 
-Omega = OmegaG * OmegaD       #nombre total de configurations possible dans l'ensemble du systeme
+        Proba.append(Omega_x[i]/Omega)
 
-ar = math.isqrt(Gammatot/Gammar)         #Air total de déplacement des boules en fonction du nombre des autres boules dans le même ring (droite)
+    return(Proba)
 
-al = math.isqrt(Gammatot/Gammal)
+x = [i for i in range(0,l+1)]
 
-A = 68*34                     #Air total de déplacement des boules en fonction du nombre des autres boules dans le même ring (droite)
+Config_Possibles = [[1, 2], [1, 3], [2, 2], [2, 3], [3, 3]]
 
-### Donnée
-
-Nb = input("la valeur de Nb")
-
-Nl = input("la valeur de Nl")
-
-Ng = input("la valeur de Ng")
-
-x =  input("position du barrau")
-
-###right or left
-
-###valeur théorique ( lattice gas model )
-
-
-For i in range(Nb) :
-    x =  input("position du barreau")
-    for j in range(Nl) :
-        lgml = al**2*(A/al)((A/al)-1)
-    for w in range(Ng):
-        lgmr = ar**2*(A/ar)((A/ar)-1)
-
-
-lgm = a**2*(A/a)((A/a)-1)
-
-
-
-
-
-
-
+for i in range(len(noms_mesures)):
+    plt.subplot(subplots[i])
+    plt.xlabel('Distance')
+    plt.ylabel('Probabilité')
+    plt.hist(mesures_distance[i], density = True, bins = 20)
+    plt.plot(x, P_stat(Config_Possibles[i]))
+    plt.title(noms_mesures[i])
+    plt.show()
 
 
 
